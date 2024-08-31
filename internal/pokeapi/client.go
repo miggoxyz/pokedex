13ceb1b68cb03422d734b0e85ac1ef9a211167e1
Ioakeim Mingo <canonshai@gmail.com>
2024-08-31 20:14:54 +0100

    Add pokecache package for caching PokeAPI responses

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,15 +3,19 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/miggoxyz/pokedex/internal/pokecache"
 )
 
 type Client struct {
+	cache pokecache.Cache
 	httpClient http.Client
 }
 
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
