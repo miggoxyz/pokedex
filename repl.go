8916bfdc79c60236c9191638ef1c01d19ccb58e0
Ioakeim Mingo <canonshai@gmail.com>
2024-09-01 13:46:33 +0100

    Implement catch, inspect, and pokedex commands.

--- repl.go
+++ repl.go
@@ -13,6 +13,7 @@ type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
 	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.Pokemon
 }
 
 
@@ -90,6 +91,21 @@ func getCommands() map[string]cliCommand {
 			name: "explore <location_name>",
 			description: "Explore a location",
 			callback: commandExplore,
+		},
+		"catch": {
+			name:        "catch <pokemon_name>",
+			description: "Attempt to catch a pokemon",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "See all the pokemon you've caught",
+			callback:    commandPokedex,
 		},
 	}
 }
